7bc801991f68abc8b825071a83fa8e429d464a6d
Brendan Malycky <bmalycky@hotmail.com>
2023-12-01 13:31:39 -0700

    added slugify

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,10 +2,21 @@
   export let title;
   export let author;
   export let tags;
+
+  function slugify(str) {
+  return String(str)
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
+    .trim() // trim leading or trailing whitespace
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+}
 </script>
 
 <div>
-  <a href="/blog/{title}">
+  <a href="/blog/{slugify(title)}">
     <article>
       <h2>{title}</h2>
       <p>{author}</p>
