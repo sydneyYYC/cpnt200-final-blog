440aab373e2a234312b752e2652e4d7108ce4158
Dorothy Nguyen <dorothyjij@gmail.com>
2023-12-02 09:33:42 -0700

    Move tailwind script import to +layout.svetle

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+<h1 class="text-blue-600">hi</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,2 @@
-<script>
-  import "../app.css";
-</script>
 
-<slot />
 <h1 class="text-blue-600">hi</h1>
