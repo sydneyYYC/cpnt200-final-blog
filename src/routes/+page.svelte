56b17024d1f4efcf139196a02787d0bd6eee1af7
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-04 00:00:42 -0700

    navbar with blogs link included

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<header class="bg-slate-900 flex justify-between p-2">
+  <div class="static w-48 h-10 text-3xl text-white mx-2"> <a href="/">Tech Insights</a></div>
+  <nav>
+    <ul class="flex justify-around gap-4">
+      <li class=" text-2xl text-white">
+          <a class="m-2 hover:text-white" href="/blog">Blogs</a>
+      </li>
+    </ul>
+  </nav>
+</header>
 <img class="brightness-75 mx-auto bg-cover" src="img/hero.jpg" alt="" />
 <section
   class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white bg-blue-950 bg-opacity-70 "
