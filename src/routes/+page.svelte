9379dd4154314c51c104306ea68ee548981bcf28
Kevin Fula <kevin.fula@edu.sait.ca>
2023-12-04 11:10:59 -0700

    added Header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,3 @@
-<header class="bg-white flex justify-between p-2">
-  <div class="static w-48 h-10 text-3xl text-black mx-2 hover:text-yellow-400"> <a href="/">Tech Insights</a></div>
-  <nav>
-    <ul class="flex justify-around gap-4">
-      <li class=" text-2xl text-black">
-          <a class="text-3xl m-2 hover:text-yellow-400" href="/blog">Blogs</a>
-      </li>
-    </ul>
-  </nav>
-</header>
 
 <img class="brightness-75 mx-auto bg-cover" src="img/hero.jpg" alt="" />
 <section
