b1baad7e5e8681933ff7d8a579ba03cdac00fc13
Sydney Bruce <sydneybruce20@gmail.com>
2023-12-04 10:58:28 -0700

    added nav and footer to blog posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,7 +5,16 @@
   // console.log(techBlog);
 </script>
 
-<!-- header goes here -->
+<header class="bg-slate-900 flex justify-between p-2">
+  <div class="static w-48 h-10 text-3xl text-white mx-2"> <a href="/">Tech Insights</a></div>
+  <nav>
+    <ul class="flex justify-around gap-4">
+      <li class=" text-2xl text-white">
+          <a class="m-2 hover:text-white" href="/blog">Blogs</a>
+      </li>
+    </ul>
+  </nav>
+</header>
 <main class="bg-blue-950 flex flex-col min-h-screen">
   <h1 class="md:text-9xl mt-[4rem] mx-4 text-white text-8xl">The Blog</h1>
   <div class="flex space-around md:items-start mt-[2rem] flex-wrap self-center">
@@ -14,7 +23,6 @@
     {/each}
   </div>
 </main>
-<!-- footer goes here -->
 
 <style>
   /* import font */
