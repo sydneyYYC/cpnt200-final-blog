23562f0481fe7900e2d46e5441a97828299dc28a
Dorothy Nguyen <dorothyjij@gmail.com>
2023-12-04 08:41:04 -0700

    trying to fun text thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
 <section
   class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white bg-blue-950 bg-opacity-70 "
 >
+<div class="typewriter text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center font-extrabold">
   <h1 class="font-libreBaskerville text-8xl text-white ">
     Welcome to Tech Insights
   </h1>
+</div>
+
   <p class="text-yellow-400 font-sourceSans mb-8 mx-8">
     Welcome to Tech Insight, your ultimate destination for rapid and insightful
     coding tutorials. At Tech Insight, we're dedicated to providing quick
