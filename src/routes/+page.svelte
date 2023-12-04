b660e8b7b2052ad6cf94d9ed121953543390e7d4
Dorothy Nguyen <dorothyjij@gmail.com>
2023-12-04 11:08:55 -0700

    merging text change to main, and styling nav a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,22 @@
   <nav>
     <ul class="flex justify-around gap-4">
       <li class=" text-2xl text-white">
-          <a class="m-2 hover:text-white" href="/blog">Blogs</a>
+          <a class="m-2 hover:text-white hover:bg-yellow-400 hover:p-2" href="/blog">Blogs</a>
       </li>
     </ul>
   </nav>
 </header>
+
 <img class="brightness-75 mx-auto bg-cover" src="img/hero.jpg" alt="" />
 <section
   class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white bg-blue-950 bg-opacity-70 "
 >
-  <h1 class="font-libreBaskerville text-8xl text-white ">
-    Welcome to Tech Insights
-  </h1>
+  <div class="typewriter text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center font-extrabold">
+    <h1 class=" typewriter font-libreBaskerville text-8xl text-white pb-4 px-2">
+      Welcome to <div>Tech Insights</div>
+    </h1>
+  </div>
+
   <p class="text-yellow-400 font-sourceSans mb-8 mx-8">
     Welcome to Tech Insight, your ultimate destination for rapid and insightful
     coding tutorials. At Tech Insight, we're dedicated to providing quick
