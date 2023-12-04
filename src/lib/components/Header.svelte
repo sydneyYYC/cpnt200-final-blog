b65eab97b8a5448754052f3d3987a2e89394ba89
Sydney Bruce <sydneybruce20@gmail.com>
2023-12-04 11:24:54 -0700

    fixed header bug

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="bg-white flex justify-between p-2">
-  <div class="static w-48 h-10 text-3xl text-black mx-2 hover:text-yellow-400 font-libreBaskerville"> <a href="/">Tech Insights</a></div>
+  <div class="static  h-10 text-3xl text-black mx-2 hover:bg-yellow-400 font-libreBaskerville"> <a href="/">Tech Insights</a></div>
   <nav>
     <ul class="flex justify-around gap-4">
       <li class=" text-2xl text-black">
