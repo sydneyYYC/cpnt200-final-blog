b62fc14444012fe7420be99fb15ad415b7927461
Dorothy Nguyen <dorothyjij@gmail.com>
2023-12-04 10:58:36 -0700

    weird text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
+<header class="bg-slate-900 flex justify-between p-2">
+  <div class="static w-48 h-10 text-3xl text-white mx-2"> <a href="/">Tech Insights</a></div>
+  <nav>
+    <ul class="flex justify-around gap-4">
+      <li class=" text-2xl text-white">
+          <a class="m-2 hover:text-white" href="/blog">Blogs</a>
+      </li>
+    </ul>
+  </nav>
+</header>
 <img class="brightness-75 mx-auto bg-cover" src="img/hero.jpg" alt="" />
 <section
   class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white bg-blue-950 bg-opacity-70 "
 >
-<div class="typewriter text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center font-libreBaskerville">
- 
-  <h1 class="font-libreBaskerville text-8xl text-white " maxlength="8" >
-    Welcome to <div>Tech Insights</div>
-  </h1>
+
+<div class="typewriter font-libreBaskerville text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center ">
+  <h1>Welcome to <div>Tech Insight</div></h1>
 </div>
 
-
-  <p class="text-yellow-400 font-sourceSans mb-8 mx-8">
+  <p class="text-yellow-400 font-sourceSans mb-8 mx-8 pt-4">
     Welcome to Tech Insight, your ultimate destination for rapid and insightful
     coding tutorials. At Tech Insight, we're dedicated to providing quick
     articles that offer hands-on guidance, helping you navigate through various
